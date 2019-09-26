59429f5f77b900f57def31ce922778d79d1815e4
Wildhevire <wildhevire404@gmail.com>
2019-09-26 18:32:59 +0000

    revisi identitas

--- src/latihan/input/Input.java
+++ src/latihan/input/Input.java
@@ -1,13 +1,13 @@
 package latihan.input;
 import java.util.Scanner;
-
+/*
+Nama        : Wildan Muhammad Fikri
+Kelas       : IF1
+NIM         : 10118044
+Deskripsi   : Memasukkan nilai dari keyboard
+ */
 public class Input {
-	/*
-    Nama        : Wildan Muhammad Fikri
-    Kelas       : IF1
-    NIM         : 10118044
-    Deskripsi   : Memasukkan nilai dari keyboard
-     */
+	
 	
 	public static void main(String[] args) {
         System.out.print("Masukkan nama anda : ");
